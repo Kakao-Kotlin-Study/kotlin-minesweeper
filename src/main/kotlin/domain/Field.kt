5d9db61d8e7a54c21c47337e1a2de83e27202620
jerrie.choi <pinkytime96@gmail.com>
2023-02-16 21:59:24 +0900

    feat: 탐색을 위한 delta 리스트 추가 및 주변 지뢰 개수 탐색 로직 추가

--- src/main/kotlin/domain/Field.kt
+++ src/main/kotlin/domain/Field.kt
@@ -2,15 +2,24 @@ package domain
 
 class Field(private val field: MutableList<MutableList<Section>>) {
 
+    private val delta = listOf(listOf(-1, -1), listOf(-1, 0), listOf(-1, 1), listOf(0, -1), listOf(0, 1), listOf(1, -1), listOf(1, 0), listOf(1, 1))
+
     fun layMine(rowNum: Int, colNum: Int): Field {
         field[rowNum][colNum] = Mine()
+        delta.forEach {
+            val newRow = rowNum + it[0]
+            val newCol = colNum + it[1]
+            if (newRow in 0 until field.size && newCol in 0 until field.size) {
+                addIfSafe(field[newRow][newCol])
+            }
+        }
         return Field(field)
     }
 
-    fun getRowString(): String {
-        val sb: StringBuilder = StringBuilder()
-        field.asSequence().forEach { sb.append(it.getString()).append("\n") }
-        return sb.toString()
+    private fun addIfSafe(section: Section) {
+        if (section is Safe) {
+            section.add()
+        }
     }
 
 }
